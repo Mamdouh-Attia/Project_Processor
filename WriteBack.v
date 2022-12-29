266bfc172d7dec42e00cb4d85c5a6f67bb184f26
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2022-12-29 19:46:01 +0200

    ALU for phase 3

--- WriteBack.v
+++
@@ -1,8 +0,0 @@
-module WriteBack (
-    input [15:0] read_data1,
-    input [15:0] read_data2,
-    output [15:0] write_data,
-    input mem_to_reg 
-);
-    assign write_data= mem_to_reg? read_data1 : read_data2;
-endmodule
