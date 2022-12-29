bebcf169fe9dfe91b8489cfc9df127611f63096c
Mamdouh-Attia <mamdouh.attia01@eng-st.cu.edu.eg>
2022-12-29 20:04:39 +0200

    merge ALU

--- ALU.v
+++ ALU.v
@@ -1,59 +1,166 @@
-module Add(input[15:0] src, input[15:0] dst, output[15:0] sum, output[3:0] flags);
-// flags -> 0 : carry, 1 : zero, 2 : negative, 3: overflow
-assign {flags[0], sum} = src + dst;
-assign flags[1] = {flags[0], sum} === 0 ? 1 : 0;
-assign flags[2] = sum[15] === 1 ? 1 : 0;
-assign flags[3] = (src[15] == dst[15] && src[15] !== sum[15]) ? 1 : 0;
-
-endmodule
-
-module Invert(input[15:0] operand, output[15:0] result, output zero_flag);
-// flags -> 0 : carry, 1 : zero, 2 : negative, 3: overflow
-assign result = ~operand;
-assign zero_flag = result === 0 ? 1 : 0;
-
-endmodule
-
-module NOP();
-endmodule
-
-module ALU_Execute(input clk, input[2:0] operation, input[15:0] src, input[15:0] dst, output reg[15:0] result, output reg[3:0] flags);
 // flags -> 0: carry, 1: zero, 2: negative, 3: overflow
+module ALU(input clk, input[4:0] alu_control, input[15:0] src, input[15:0] dst, output reg[15:0] result, output reg[3:0] flags);
 
- always @ (*) begin
- 	if(operation === 3'b000)	// Add
+	
+ always @ (posedge clk)
+  begin
+ 	if(alu_control === 0)	// NOP
 	begin
-	{flags[0], result} = src + dst;
-	flags[1] =  result === 0 ? 1 : 0;
-	flags[2] = result[15] === 1 ? 1 : 0;
-	flags[3] = (src[15] == dst[15] && src[15] !== result[15]) ? 1 : 0;
+		// nothing
 	end
 
-	if(operation == 3'b001)		// Invert
+	else if(alu_control === 1)		// SETC
 	begin
-	result = ~src;
-	flags[0] =  0;
-	flags[1] = result === 0 ? 1 : 0;
-	flags[2] =  0;
-	flags[3] =  0;
+		flags[0] = 1;
 	end
 
-	if(operation == 3'b100)		// NOP
+	else if(alu_control === 2)		// CLRC
 	begin
-
-	 end
-
-	if(operation === 3'b011)	// Store
-	begin
-	result = dst;
+		flags[0] = 0;
 	end
 
-	if(operation === 3'b010)	// Load
+	else if(alu_control === 3)	// NOT
 	begin
-	{flags[0], result} = src + dst;
-	flags[1] =  result === 0 ? 1 : 0;
-	flags[2] = result[15] === 1 ? 1 : 0;
-	flags[3] = (src[15] == dst[15] && src[15] !== result[15]) ? 1 : 0;
+		result = ~dst;
+		flags[1] = result == 0 ? 1 : 0;
+		flags[2] = result[15] == 1 ? 1 : 0;
+	end
+
+	else if(alu_control === 4)	// INC
+	begin
+		result = dst + 1;
+		flags[1] =  result === 0 ? 1 : 0;
+		flags[2] = result[15] === 1 ? 1 : 0;
+	end
+
+	else if(alu_control === 5)			// DEC
+	begin
+		result = dst - 1;
+		flags[1] =  result === 0 ? 1 : 0;
+		flags[2] = result[15] === 1 ? 1 : 0;
+	end
+
+	else if(alu_control === 6)			// OUT
+	begin
+		// nothing
+	end
+
+	else if(alu_control === 7)			// IN
+	begin
+		// nothing
+	end
+
+	else if(alu_control === 8)			// MOV
+	begin
+		result = src;
+	end
+
+	else if(alu_control === 9)			// ADD
+	begin
+		{flags[0], result} = src + dst;
+		flags[1] =  result === 0 ? 1 : 0;
+		flags[2] = result[15] === 1 ? 1 : 0;
+		flags[3] = (src[15] === dst[15] && src[15] != result[15]) ? 1 : 0;
+	end
+
+	else if(alu_control === 10)		// SUB
+	begin
+		result = src - dst;
+		flags[1] =  result === 0 ? 1 : 0;
+		flags[2] = result[15] === 1 ? 1 : 0;
+		//If 2 numbers are subtracted, and their signs are different, then overflow occurs if and only if the result has the same sign as the subtrahend.
+		flags[3] = (src[15]^dst[15] === 1 && result[15] === dst[15]) ? 1 : 0;
+	end
+
+	else if(alu_control === 11)		// AND
+	begin
+		result = src & dst;
+		flags[1] =  result === 0 ? 1 : 0;
+		flags[2] = result[15] === 1 ? 1 : 0;
+	end
+
+	else if(alu_control === 12)		// OR
+	begin
+		result = src | dst;
+		flags[1] =  result === 0 ? 1 : 0;
+		flags[2] = result[15] === 1 ? 1 : 0;
+	end
+
+	else if(alu_control === 13)		// SHL
+	begin
+	// note : dst here = immediate value
+		{flags[0], result} = src <<< dst;
+	end
+
+	else if(alu_control === 14)		// SHR
+	begin
+	// note : dst here = immediate value
+		{result, flags[0]} = src <<< dst;
+	end
+
+	else if(alu_control === 15)		// PUSH
+	begin
+	// note : src here = SP
+		result = src;
+	end
+
+	else if(alu_control === 16)		// POP
+	begin
+	// note : src here = SP
+		result = src;
+	end
+
+	else if(alu_control === 17)		// LDM
+	begin
+	// note : src here = immediate value
+		result = src;
+	end
+
+	else if(alu_control === 18)		// LDD
+	begin
+		// nothing
+	end
+
+	else if(alu_control === 19)		// STD
+	begin
+		// nothing
+	end
+
+	else if(alu_control === 20)		// JZ
+	begin
+
+	end
+
+	else if(alu_control === 21)		// JN
+	begin
+
+	end
+
+	else if(alu_control === 22)		// JC
+	begin
+
+	end
+
+	else if(alu_control === 23)		// JMP
+	begin
+
+	end
+
+	else if(alu_control === 24)		// CALL
+	begin
+
+	end
+
+	else if(alu_control === 25)		// RET
+	begin
+
+	end
+
+	else if(alu_control === 26)		// RETI
+	begin
+
 	end
  end
 endmodule
+
+
